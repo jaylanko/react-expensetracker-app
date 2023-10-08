f182b7daa212be1003a9f1bf17c7312f3e403cf5
jaylanko <dzejlan.husovic@hotmail.com>
2023-10-08 16:58:49 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Block from "./components/Block";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started right now!</h2>
+      <ExpenseItem></ExpenseItem>
+      <Block></Block>
+      <p>This is also visibleeee?</p>
+    </div>
+  );
+}
+
+export default App;
