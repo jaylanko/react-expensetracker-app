d7fba1e0608a094221ae68edbf5665e8c4e6470d
jaylanko <dzejlan.husovic@hotmail.com>
2023-10-08 18:09:54 +0200

    Used props to pass data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
-import './ExpenseItem.css';
-function ExpenseItem() {
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
   return (
-  <div className="expense-item">
-    <div>October 8th 2023</div>
-    <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-  </div>
   );
 }
 
