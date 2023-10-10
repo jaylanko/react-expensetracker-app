6952f6d8ee2ad5cc8b18daa24652d88659f54d21
jaylanko <dzejlan.husovic@hotmail.com>
2023-10-10 17:02:33 +0200

    Put expenseitem into Expenses component

---
+++ src/components/Expenses.js
@@ -0,0 +1,34 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+
+function Expenses(props) {
+  const expense = props.expense;
+
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
