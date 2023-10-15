24a759a8541e2a70eb17a8d4e4cf62788c61642a
jaylanko <dzejlan.husovic@hotmail.com>
2023-10-15 14:24:15 +0200

    adding event handlers for input forms

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,37 @@
 import "./ExpenseForm.css";
 
+import React, {useState} from 'react';
+
 const ExpenseForm = () => {
+    const [enteredNumber, setEnteredNumber] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const numberChangeHandler = (event) => {
+        setEnteredNumber(event.target.value);
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={numberChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2023-01-01" max="2024-12-31" />
+          <input type="date" min="2023-01-01" max="2024-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
